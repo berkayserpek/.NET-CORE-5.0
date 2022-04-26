db0bd93cdbba79a4f84198169b929b0c90d12738
Berkay Serpek <berkay.serpek@toptalent.co>
2022-04-26 17:17:27 +0300

    testimonial crud

---
+++ Core_Proje/Controllers/TestimonialController.cs
@@ -0,0 +1,39 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core_Proje.Controllers
+{
+    public class TestimonialController : Controller
+    {
+        TestimonialManager testimonialManager = new TestimonialManager(new EFTestimonialDAL());
+        public IActionResult Index()
+        {
+            var values = testimonialManager.TGetList();
+            return View(values);
+        }
+        public IActionResult DeleteTestimonial(int id)
+        {
+            var values = testimonialManager.TGetByID(id);
+            testimonialManager.TDelete(values);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult UpdateTestimonial(int id)
+        {
+            var values = testimonialManager.TGetByID(id);
+            return View(values);
+        }
+        [HttpPost]
+        public IActionResult UpdateTestimonial(Testimonial testimonial)
+        {
+            testimonialManager.TUpdate(testimonial);
+            return RedirectToAction("Index");
+        }
+    }
+}
